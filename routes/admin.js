5e16d5a06f06b55e3b7105e3e339032043491d77
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-04-21 16:22:28 -0400

    Added handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 // /admin - routes
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+   //  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+   res.render('add-product', {pageTitle: 'Add product'});
  });
 
  router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -4,12 +4,14 @@ const path = require('path');
 
 const rootDir = require('../helpers/path');
 
+// Bring in admin page data (temp)
 const adminData = require('./admin');
 
 // "/" - routes
 router.get("/", function (req, res, next) {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, "views", "shop.html"));
+    console.log('Available products: ', adminData.products);
+    // res.sendFile(path.join(rootDir, "views", "shop.html"));
+    res.render('shop', { pageTitle: 'Shop' });
 });
 
 module.exports = router; 
