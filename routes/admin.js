bcd08c0c58bdec74413ef2dedf412732980f4c9c
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-04-22 00:11:12 -0400

    Handlebars and ui changes, login updates to shop and admin scripts

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,7 @@
-// Admin routes for creating shop products
-
+// Admin routes for admin users
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
 const rootDir = require('../helpers/path');
 
 const products = [];
@@ -11,11 +9,11 @@ const products = [];
 // /admin - routes
 router.get('/add-product', (req, res, next) => {
    //  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-   res.render('add-product', {pageTitle: 'Add product'});
+   res.render('add-product', {pageTitle: 'Add product', activeAddProduct: true});
  });
 
  router.post("/add-product", (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({title: req.body.title, description: req.body.description});
     res.redirect('/'); // Auto rediredt page to url on req
  });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
 const rootDir = require('../helpers/path');
 
 // Bring in admin page data (temp)
@@ -11,7 +10,7 @@ const adminData = require('./admin');
 router.get("/", function (req, res, next) {
     console.log('Available products: ', adminData.products);
     // res.sendFile(path.join(rootDir, "views", "shop.html"));
-    res.render('shop', { pageTitle: 'Shop' });
+    res.render('shop', { pageTitle: 'Shop', products: adminData.products, haveProducts: adminData.products.length > 0, activeShop: true});
 });
 
 module.exports = router; 
